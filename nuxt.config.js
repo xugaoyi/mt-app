a3910575e05557c2479e53d819162db0abb4106b
xugaoyi <894072666@qq.com>
2019-07-19 18:08:17 +0800

    首页开发

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,9 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/reset.css',
+    'element-ui/lib/theme-chalk/index.css',
+    '@/assets/css/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -54,6 +56,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-    }
+    },
+    cache: true
   }
 }
